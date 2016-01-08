3289d486d0b1087f21cdf5f1fa6705c70c04b4d7
TangZhihao <tjrtzh@gmail.com>
2016-01-08 21:36:39 +0800

    update
    new update
    no bug report

--- html/php/asm.php
+++ html/php/asm.php
@@ -1,14 +1,10 @@
 <?php
     if(isset($_POST['code']))
 	{
-		$myfile = fopen("input.c", "w") or die("Unable to open file!");
+		$myfile = fopen("output/code.asm", "w") or die("Unable to open file!");
 		fwrite($myfile, $_POST['code']);
 		fclose($myfile);
 		
-		system("python compiler.py");
-		
-		copy("code.asm","output/code.asm");
-	
 		system("CPU.exe");
 		
 		system("7z a -tzip coe.zip output/");
